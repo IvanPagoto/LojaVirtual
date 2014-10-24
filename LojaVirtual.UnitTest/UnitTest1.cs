444c76bc9b9390d7af740f3ae1e8638160d4208b
ivanpagoto <ivanpagoto@gmail.com>
2014-10-24 09:01:57 -0300

    COmits

--- LojaVirtual.UnitTest/UnitTest1.cs
+++ LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
+﻿using LojaVirtual.Web.HtmlHelpers;
+using LojaVirtual.Web.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace LojaVirtual.UnitTest
 {
@@ -33,6 +35,36 @@ namespace LojaVirtual.UnitTest
             int[] teste = { 1, 3, 9 };
 
             CollectionAssert.AreEqual(resultado.ToArray(), teste);
+        }
+
+        [TestMethod]
+        public void TestarSePaginacaoEstaSendogeradaCorretamente()
+        {
+            //Arrange
+            HtmlHelper html = null;
+
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensPorPagina = 10,
+                ItensTotal = 28
+
+            };
+
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+            //Act
+            MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
+
+
+            //MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
+
+            //Assert
+            Assert.AreEqual(
+                 @"<a class=""btn btn-default"" href=""Pagina1"">1</a>"
+               + @"<a class=""btn btn-default btn-primary selected"" href=""Pagina2"">2</a>"
+               + @"<a class=""btn btn-default"" href=""Pagina1"">3</a>", resultado.ToString()
+                 );
         }
 
     }
